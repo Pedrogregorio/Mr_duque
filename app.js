ed0dcfa6642c76046cd2d3fc1f50ce1d9df540ae
Pedrogregorio <pedrogregoriodias675@gmail.com>
2021-01-11 18:12:55 -0400

    Teste de Deploy

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const app = require('./config/config')
 const port = 3000
 
-app.listen(port, ()=>{
-    console.log(`app rodando na porta http://localhost:${port}`)
+app.listen(process.env.PORT || port, function(){
+    console.log('serve on')
 })
