a22e1b42a5093592936633bd844284cf16f9095b
Pedrogregorio <pedrogregoriodias675@gmail.com>
2021-01-12 21:19:52 -0400

    Verificando Erro de Rota

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -11,36 +11,7 @@ module.exports = function(app) {
 		check('senha', 'Preencha o campo Senha ').not().isEmpty()
 	],
     function(req, res) {
-    const dadosForm = req.body
-    const errors = validationResult(req);
-    const validacao = errors.array() 
-    if (!errors.isEmpty()) {
-        res.render('index', {validacao, dadosForm: dadosForm })
-    }else{
-         const conn = app.config.dbConfig
-         const indexDAO = new app.app.models.indexDAO(conn)
-         indexDAO.autenticar(dadosForm, function(err, result) {
-            if(result[0] != undefined){
-                req.session.autorizado = true
-                req.session.nome = result[0].nome_usuario
-                req.session._id = result[0].id_usuario 
-                req.session.tipo = result[0].tipo 
-                
-                if(req.session.tipo == 1){ 
-                    res.redirect('/dashboardAdm')
-                }else if (req.session.tipo == 2){
-                    res.redirect('/dashboard')
-                }
-            }
-            else{
-                res.render('index', {
-                    validacao:[
-                        {msg:'Erro: Usuario ou Senha incorretos'}
-                    ]
-                })
-            }   
-        });
-    }
+        app.app.controllers.index.validacao(app, req, res)
     })
     app.get('/validacao', function(req, res) {
         
