97c14646ccc94259061b1cc0ac119626b08845c3
Pedrogregorio <pedrogregoriodias675@gmail.com>
2021-01-13 00:00:56 -0400

    Verificando Erro de Rota

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,13 +1,13 @@
 const { check } = require('express-validator')
 module.exports = function(app) {
     
+    app.get('/', function(req, res) {    
+        res.render('index', {validacao:{}})
+    })
+
     app.use(function (err, req, res, next) {
         console.error(err.stack)
         res.status(500).send('Something broke!')
-    })
-
-    app.get('/', function(req, res) {    
-        res.render('index', {validacao:{}})
     })
 
     app.post('/validacao', 
@@ -15,8 +15,9 @@ module.exports = function(app) {
 		check('nome', 'Preencha o campo Nome ').not().isEmpty(),
 		check('senha', 'Preencha o campo Senha ').not().isEmpty()
 	],
-    function(req, res) {
+    function(req, res, next) {
         app.app.controllers.index.validacao(app, req, res)
+        next()
     })
     app.get('/validacao', function(req, res) {
         
