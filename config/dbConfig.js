eb3aadfabbf37f4b7062ab4a1fa18d60e1201e8c
Pedrogregorio <pedrogregoriodias675@gmail.com>
2021-01-13 00:43:28 -0400

    Verificando Erro de Rota

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,24 +1,31 @@
 const mysql = require('mysql')
 
 const connMysql = function(err) {
-    console.log('entrou na connexao')
     const connection =  mysql.createConnection({
-        host: 'mr-duque.herokuapp.com',
+
+        host: '50.116.112.120',
 		user: 'teren671_pedro',
 		password: 'BctR20m4b8',
 		database: 'teren671_mr_duque'
 
     })
-    // connection.on('error', err => {
-    //     if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-    //         // db error reconnect
-    //         connMysql();
-    //     } else {
-    //         throw err;
-    //     }
-    // }) 
-    
-    return connection    
+    connection.on('error', err => {
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+            // db error reconnect
+            connMysql();
+        } else {
+            throw err;
+        }
+    });
+    connection.connect(function(err) {
+        if (err) {
+          console.error('error connecting pohhhhhhhhhhha: ' + err.stack);
+          return;
+        }else{
+            console.log('Conectado Birr')
+        }
+      })
+      return connection    
 }
 
 module.exports = function () {
